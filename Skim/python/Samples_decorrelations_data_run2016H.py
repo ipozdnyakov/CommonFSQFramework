a8cd47a3baaab5a4eb0332ba7689701faedc3405
Ivan <ivanp@cern.ch>
2017-05-03 13:07:36 +0200

    skimms for 2016 data done

--- Skim/python/Samples_decorrelations_data_run2016H.py
+++ Skim/python/Samples_decorrelations_data_run2016H.py
@@ -14,9 +14,12 @@ sam["data_FSQJets"]["GT"]='80X_dataRun2_2016SeptRepro_v7'
 sam["data_FSQJets"]["name"]='data_FSQJets'
 sam["data_FSQJets"]["isData"]=True
 sam["data_FSQJets"]["numEvents"]=-1
+sam["data_FSQJets"]["pathSE"]='srm://se3.itep.ru:8443/srm/managerv2?SFN=/pnfs/itep.ru/data/cms/store/user/ivanp/FSQJets/decorrelations_data_run2016H_data_FSQJets/170502_153142/0000/'
+sam["data_FSQJets"]["pathTrees"]='/XXXTMFTTree/store/user/ivanp/FSQJets/decorrelations_data_run2016H_data_FSQJets/170502_153142/0000//'
 sam["data_FSQJets"]["json"]='CommonFSQFramework/Skim/lumi/Cert_271036-284044_13TeV_PromptReco_Collisions16_JSON_LowPU.txt'
 sam["data_FSQJets"]["lumiMinBias"]=-1
 sam["data_FSQJets"]["XS"]=-1
+sam["data_FSQJets"]["pathPAT"]='/XXXTMFPAT/store/user/ivanp/FSQJets/decorrelations_data_run2016H_data_FSQJets/170502_153142/0000//'
 sam["data_FSQJets"]["DS"]='/FSQJets/Run2016H-03Feb2017_ver2-v1/MINIAOD'
 
 sam["data_L1MinimumBias"]={}
@@ -25,9 +28,12 @@ sam["data_L1MinimumBias"]["GT"]='80X_dataRun2_Prompt_v16'
 sam["data_L1MinimumBias"]["name"]='data_L1MinimumBias'
 sam["data_L1MinimumBias"]["isData"]=True
 sam["data_L1MinimumBias"]["numEvents"]=-1
+sam["data_L1MinimumBias"]["pathSE"]='srm://se3.itep.ru:8443/srm/managerv2?SFN=/pnfs/itep.ru/data/cms/store/user/ivanp/L1MinimumBias/decorrelations_data_run2016H_data_L1MinimumBias/170502_153201/0000/'
+sam["data_L1MinimumBias"]["pathTrees"]='/XXXTMFTTree/store/user/ivanp/L1MinimumBias/decorrelations_data_run2016H_data_L1MinimumBias/170502_153201/0000//'
 sam["data_L1MinimumBias"]["json"]='CommonFSQFramework/Skim/lumi/Cert_271036-284044_13TeV_PromptReco_Collisions16_JSON_LowPU.txt'
 sam["data_L1MinimumBias"]["lumiMinBias"]=-1
 sam["data_L1MinimumBias"]["XS"]=-1
+sam["data_L1MinimumBias"]["pathPAT"]='/XXXTMFPAT/store/user/ivanp/L1MinimumBias/decorrelations_data_run2016H_data_L1MinimumBias/170502_153201/0000//'
 sam["data_L1MinimumBias"]["DS"]='/L1MinimumBias/Run2016H-PromptReco-v2/MINIAOD'
 
 sam["data_L1MinimumBias0"]={}
@@ -36,9 +42,12 @@ sam["data_L1MinimumBias0"]["GT"]='80X_dataRun2_Prompt_v16'
 sam["data_L1MinimumBias0"]["name"]='data_L1MinimumBias0'
 sam["data_L1MinimumBias0"]["isData"]=True
 sam["data_L1MinimumBias0"]["numEvents"]=-1
+sam["data_L1MinimumBias0"]["pathSE"]='srm://se3.itep.ru:8443/srm/managerv2?SFN=/pnfs/itep.ru/data/cms/store/user/ivanp/L1MinimumBias0/decorrelations_data_run2016H_data_L1MinimumBias0/170502_153243/0000/'
+sam["data_L1MinimumBias0"]["pathTrees"]='/XXXTMFTTree/store/user/ivanp/L1MinimumBias0/decorrelations_data_run2016H_data_L1MinimumBias0/170502_153243/0000//'
 sam["data_L1MinimumBias0"]["json"]='CommonFSQFramework/Skim/lumi/Cert_271036-284044_13TeV_PromptReco_Collisions16_JSON_LowPU.txt'
 sam["data_L1MinimumBias0"]["lumiMinBias"]=-1
 sam["data_L1MinimumBias0"]["XS"]=-1
+sam["data_L1MinimumBias0"]["pathPAT"]='/XXXTMFPAT/store/user/ivanp/L1MinimumBias0/decorrelations_data_run2016H_data_L1MinimumBias0/170502_153243/0000//'
 sam["data_L1MinimumBias0"]["DS"]='/L1MinimumBias0/Run2016H-PromptReco-v2/MINIAOD'
 
 sam["data_L1MinimumBias1"]={}
@@ -47,9 +56,12 @@ sam["data_L1MinimumBias1"]["GT"]='80X_dataRun2_Prompt_v16'
 sam["data_L1MinimumBias1"]["name"]='data_L1MinimumBias1'
 sam["data_L1MinimumBias1"]["isData"]=True
 sam["data_L1MinimumBias1"]["numEvents"]=-1
+sam["data_L1MinimumBias1"]["pathSE"]='srm://se3.itep.ru:8443/srm/managerv2?SFN=/pnfs/itep.ru/data/cms/store/user/ivanp/L1MinimumBias1/decorrelations_data_run2016H_data_L1MinimumBias1/170502_153252/0000/'
+sam["data_L1MinimumBias1"]["pathTrees"]='/XXXTMFTTree/store/user/ivanp/L1MinimumBias1/decorrelations_data_run2016H_data_L1MinimumBias1/170502_153252/0000//'
 sam["data_L1MinimumBias1"]["json"]='CommonFSQFramework/Skim/lumi/Cert_271036-284044_13TeV_PromptReco_Collisions16_JSON_LowPU.txt'
 sam["data_L1MinimumBias1"]["lumiMinBias"]=-1
 sam["data_L1MinimumBias1"]["XS"]=-1
+sam["data_L1MinimumBias1"]["pathPAT"]='/XXXTMFPAT/store/user/ivanp/L1MinimumBias1/decorrelations_data_run2016H_data_L1MinimumBias1/170502_153252/0000//'
 sam["data_L1MinimumBias1"]["DS"]='/L1MinimumBias1/Run2016H-PromptReco-v2/MINIAOD'
 
 sam["data_L1MinimumBias2"]={}
@@ -58,9 +70,12 @@ sam["data_L1MinimumBias2"]["GT"]='80X_dataRun2_Prompt_v16'
 sam["data_L1MinimumBias2"]["name"]='data_L1MinimumBias2'
 sam["data_L1MinimumBias2"]["isData"]=True
 sam["data_L1MinimumBias2"]["numEvents"]=-1
+sam["data_L1MinimumBias2"]["pathSE"]='srm://se3.itep.ru:8443/srm/managerv2?SFN=/pnfs/itep.ru/data/cms/store/user/ivanp/L1MinimumBias2/decorrelations_data_run2016H_data_L1MinimumBias2/170502_153303/0000/'
+sam["data_L1MinimumBias2"]["pathTrees"]='/XXXTMFTTree/store/user/ivanp/L1MinimumBias2/decorrelations_data_run2016H_data_L1MinimumBias2/170502_153303/0000//'
 sam["data_L1MinimumBias2"]["json"]='CommonFSQFramework/Skim/lumi/Cert_271036-284044_13TeV_PromptReco_Collisions16_JSON_LowPU.txt'
 sam["data_L1MinimumBias2"]["lumiMinBias"]=-1
 sam["data_L1MinimumBias2"]["XS"]=-1
+sam["data_L1MinimumBias2"]["pathPAT"]='/XXXTMFPAT/store/user/ivanp/L1MinimumBias2/decorrelations_data_run2016H_data_L1MinimumBias2/170502_153303/0000//'
 sam["data_L1MinimumBias2"]["DS"]='/L1MinimumBias2/Run2016H-PromptReco-v2/MINIAOD'
 
 sam["data_L1MinimumBias3"]={}
@@ -69,9 +84,12 @@ sam["data_L1MinimumBias3"]["GT"]='80X_dataRun2_Prompt_v16'
 sam["data_L1MinimumBias3"]["name"]='data_L1MinimumBias3'
 sam["data_L1MinimumBias3"]["isData"]=True
 sam["data_L1MinimumBias3"]["numEvents"]=-1
+sam["data_L1MinimumBias3"]["pathSE"]='srm://se3.itep.ru:8443/srm/managerv2?SFN=/pnfs/itep.ru/data/cms/store/user/ivanp/L1MinimumBias3/decorrelations_data_run2016H_data_L1MinimumBias3/170502_153312/0000/'
+sam["data_L1MinimumBias3"]["pathTrees"]='/XXXTMFTTree/store/user/ivanp/L1MinimumBias3/decorrelations_data_run2016H_data_L1MinimumBias3/170502_153312/0000//'
 sam["data_L1MinimumBias3"]["json"]='CommonFSQFramework/Skim/lumi/Cert_271036-284044_13TeV_PromptReco_Collisions16_JSON_LowPU.txt'
 sam["data_L1MinimumBias3"]["lumiMinBias"]=-1
 sam["data_L1MinimumBias3"]["XS"]=-1
+sam["data_L1MinimumBias3"]["pathPAT"]='/XXXTMFPAT/store/user/ivanp/L1MinimumBias3/decorrelations_data_run2016H_data_L1MinimumBias3/170502_153312/0000//'
 sam["data_L1MinimumBias3"]["DS"]='/L1MinimumBias3/Run2016H-PromptReco-v2/MINIAOD'
 
 sam["data_L1MinimumBias4"]={}
@@ -80,9 +98,12 @@ sam["data_L1MinimumBias4"]["GT"]='80X_dataRun2_Prompt_v16'
 sam["data_L1MinimumBias4"]["name"]='data_L1MinimumBias4'
 sam["data_L1MinimumBias4"]["isData"]=True
 sam["data_L1MinimumBias4"]["numEvents"]=-1
+sam["data_L1MinimumBias4"]["pathSE"]='srm://se3.itep.ru:8443/srm/managerv2?SFN=/pnfs/itep.ru/data/cms/store/user/ivanp/L1MinimumBias4/decorrelations_data_run2016H_data_L1MinimumBias4/170502_153210/0000/'
+sam["data_L1MinimumBias4"]["pathTrees"]='/XXXTMFTTree/store/user/ivanp/L1MinimumBias4/decorrelations_data_run2016H_data_L1MinimumBias4/170502_153210/0000//'
 sam["data_L1MinimumBias4"]["json"]='CommonFSQFramework/Skim/lumi/Cert_271036-284044_13TeV_PromptReco_Collisions16_JSON_LowPU.txt'
 sam["data_L1MinimumBias4"]["lumiMinBias"]=-1
 sam["data_L1MinimumBias4"]["XS"]=-1
+sam["data_L1MinimumBias4"]["pathPAT"]='/XXXTMFPAT/store/user/ivanp/L1MinimumBias4/decorrelations_data_run2016H_data_L1MinimumBias4/170502_153210/0000//'
 sam["data_L1MinimumBias4"]["DS"]='/L1MinimumBias4/Run2016H-PromptReco-v2/MINIAOD'
 
 sam["data_L1MinimumBias5"]={}
@@ -91,9 +112,12 @@ sam["data_L1MinimumBias5"]["GT"]='80X_dataRun2_Prompt_v16'
 sam["data_L1MinimumBias5"]["name"]='data_L1MinimumBias5'
 sam["data_L1MinimumBias5"]["isData"]=True
 sam["data_L1MinimumBias5"]["numEvents"]=-1
+sam["data_L1MinimumBias5"]["pathSE"]='srm://se3.itep.ru:8443/srm/managerv2?SFN=/pnfs/itep.ru/data/cms/store/user/ivanp/L1MinimumBias5/decorrelations_data_run2016H_data_L1MinimumBias5/170502_153218/0000/'
+sam["data_L1MinimumBias5"]["pathTrees"]='/XXXTMFTTree/store/user/ivanp/L1MinimumBias5/decorrelations_data_run2016H_data_L1MinimumBias5/170502_153218/0000//'
 sam["data_L1MinimumBias5"]["json"]='CommonFSQFramework/Skim/lumi/Cert_271036-284044_13TeV_PromptReco_Collisions16_JSON_LowPU.txt'
 sam["data_L1MinimumBias5"]["lumiMinBias"]=-1
 sam["data_L1MinimumBias5"]["XS"]=-1
+sam["data_L1MinimumBias5"]["pathPAT"]='/XXXTMFPAT/store/user/ivanp/L1MinimumBias5/decorrelations_data_run2016H_data_L1MinimumBias5/170502_153218/0000//'
 sam["data_L1MinimumBias5"]["DS"]='/L1MinimumBias5/Run2016H-PromptReco-v2/MINIAOD'
 
 sam["data_L1MinimumBias6"]={}
@@ -102,9 +126,12 @@ sam["data_L1MinimumBias6"]["GT"]='80X_dataRun2_Prompt_v16'
 sam["data_L1MinimumBias6"]["name"]='data_L1MinimumBias6'
 sam["data_L1MinimumBias6"]["isData"]=True
 sam["data_L1MinimumBias6"]["numEvents"]=-1
+sam["data_L1MinimumBias6"]["pathSE"]='srm://se3.itep.ru:8443/srm/managerv2?SFN=/pnfs/itep.ru/data/cms/store/user/ivanp/L1MinimumBias6/decorrelations_data_run2016H_data_L1MinimumBias6/170502_153226/0000/'
+sam["data_L1MinimumBias6"]["pathTrees"]='/XXXTMFTTree/store/user/ivanp/L1MinimumBias6/decorrelations_data_run2016H_data_L1MinimumBias6/170502_153226/0000//'
 sam["data_L1MinimumBias6"]["json"]='CommonFSQFramework/Skim/lumi/Cert_271036-284044_13TeV_PromptReco_Collisions16_JSON_LowPU.txt'
 sam["data_L1MinimumBias6"]["lumiMinBias"]=-1
 sam["data_L1MinimumBias6"]["XS"]=-1
+sam["data_L1MinimumBias6"]["pathPAT"]='/XXXTMFPAT/store/user/ivanp/L1MinimumBias6/decorrelations_data_run2016H_data_L1MinimumBias6/170502_153226/0000//'
 sam["data_L1MinimumBias6"]["DS"]='/L1MinimumBias6/Run2016H-PromptReco-v2/MINIAOD'
 
 sam["data_L1MinimumBias7"]={}
@@ -113,9 +140,12 @@ sam["data_L1MinimumBias7"]["GT"]='80X_dataRun2_Prompt_v16'
 sam["data_L1MinimumBias7"]["name"]='data_L1MinimumBias7'
 sam["data_L1MinimumBias7"]["isData"]=True
 sam["data_L1MinimumBias7"]["numEvents"]=-1
+sam["data_L1MinimumBias7"]["pathSE"]='srm://se3.itep.ru:8443/srm/managerv2?SFN=/pnfs/itep.ru/data/cms/store/user/ivanp/L1MinimumBias7/decorrelations_data_run2016H_data_L1MinimumBias7/170502_153233/0000/'
+sam["data_L1MinimumBias7"]["pathTrees"]='/XXXTMFTTree/store/user/ivanp/L1MinimumBias7/decorrelations_data_run2016H_data_L1MinimumBias7/170502_153233/0000//'
 sam["data_L1MinimumBias7"]["json"]='CommonFSQFramework/Skim/lumi/Cert_271036-284044_13TeV_PromptReco_Collisions16_JSON_LowPU.txt'
 sam["data_L1MinimumBias7"]["lumiMinBias"]=-1
 sam["data_L1MinimumBias7"]["XS"]=-1
+sam["data_L1MinimumBias7"]["pathPAT"]='/XXXTMFPAT/store/user/ivanp/L1MinimumBias7/decorrelations_data_run2016H_data_L1MinimumBias7/170502_153233/0000//'
 sam["data_L1MinimumBias7"]["DS"]='/L1MinimumBias7/Run2016H-PromptReco-v2/MINIAOD'
 
 sam["data_L1MinimumBias8"]={}
@@ -124,9 +154,12 @@ sam["data_L1MinimumBias8"]["GT"]='80X_dataRun2_Prompt_v16'
 sam["data_L1MinimumBias8"]["name"]='data_L1MinimumBias8'
 sam["data_L1MinimumBias8"]["isData"]=True
 sam["data_L1MinimumBias8"]["numEvents"]=-1
+sam["data_L1MinimumBias8"]["pathSE"]='srm://se3.itep.ru:8443/srm/managerv2?SFN=/pnfs/itep.ru/data/cms/store/user/ivanp/L1MinimumBias8/decorrelations_data_run2016H_data_L1MinimumBias8/170502_153320/0000/'
+sam["data_L1MinimumBias8"]["pathTrees"]='/XXXTMFTTree/store/user/ivanp/L1MinimumBias8/decorrelations_data_run2016H_data_L1MinimumBias8/170502_153320/0000//'
 sam["data_L1MinimumBias8"]["json"]='CommonFSQFramework/Skim/lumi/Cert_271036-284044_13TeV_PromptReco_Collisions16_JSON_LowPU.txt'
 sam["data_L1MinimumBias8"]["lumiMinBias"]=-1
 sam["data_L1MinimumBias8"]["XS"]=-1
+sam["data_L1MinimumBias8"]["pathPAT"]='/XXXTMFPAT/store/user/ivanp/L1MinimumBias8/decorrelations_data_run2016H_data_L1MinimumBias8/170502_153320/0000//'
 sam["data_L1MinimumBias8"]["DS"]='/L1MinimumBias8/Run2016H-PromptReco-v2/MINIAOD'
 
 sam["data_L1MinimumBias9"]={}
@@ -135,32 +168,13 @@ sam["data_L1MinimumBias9"]["GT"]='80X_dataRun2_Prompt_v16'
 sam["data_L1MinimumBias9"]["name"]='data_L1MinimumBias9'
 sam["data_L1MinimumBias9"]["isData"]=True
 sam["data_L1MinimumBias9"]["numEvents"]=-1
+sam["data_L1MinimumBias9"]["pathSE"]='srm://se3.itep.ru:8443/srm/managerv2?SFN=/pnfs/itep.ru/data/cms/store/user/ivanp/L1MinimumBias9/decorrelations_data_run2016H_data_L1MinimumBias9/170502_153329/0000/'
+sam["data_L1MinimumBias9"]["pathTrees"]='/XXXTMFTTree/store/user/ivanp/L1MinimumBias9/decorrelations_data_run2016H_data_L1MinimumBias9/170502_153329/0000//'
 sam["data_L1MinimumBias9"]["json"]='CommonFSQFramework/Skim/lumi/Cert_271036-284044_13TeV_PromptReco_Collisions16_JSON_LowPU.txt'
 sam["data_L1MinimumBias9"]["lumiMinBias"]=-1
 sam["data_L1MinimumBias9"]["XS"]=-1
+sam["data_L1MinimumBias9"]["pathPAT"]='/XXXTMFPAT/store/user/ivanp/L1MinimumBias9/decorrelations_data_run2016H_data_L1MinimumBias9/170502_153329/0000//'
 sam["data_L1MinimumBias9"]["DS"]='/L1MinimumBias9/Run2016H-PromptReco-v2/MINIAOD'
-
-sam["data_MinimumBias"]={}
-sam["data_MinimumBias"]["crabJobs"]=50
-sam["data_MinimumBias"]["GT"]='80X_dataRun2_Prompt_v16'
-sam["data_MinimumBias"]["name"]='data_MinimumBias'
-sam["data_MinimumBias"]["isData"]=True
-sam["data_MinimumBias"]["numEvents"]=-1
-sam["data_MinimumBias"]["json"]='CommonFSQFramework/Skim/lumi/Cert_271036-284044_13TeV_PromptReco_Collisions16_JSON_LowPU.txt'
-sam["data_MinimumBias"]["lumiMinBias"]=-1
-sam["data_MinimumBias"]["XS"]=-1
-sam["data_MinimumBias"]["DS"]='/MinimumBias/Run2016H-PromptReco-v3/MINIAOD'
-
-sam["data_ZeroBias"]={}
-sam["data_ZeroBias"]["crabJobs"]=50
-sam["data_ZeroBias"]["GT"]='80X_dataRun2_2016SeptRepro_v7'
-sam["data_ZeroBias"]["name"]='data_ZeroBias'
-sam["data_ZeroBias"]["isData"]=True
-sam["data_ZeroBias"]["numEvents"]=-1
-sam["data_ZeroBias"]["json"]='CommonFSQFramework/Skim/lumi/Cert_271036-284044_13TeV_PromptReco_Collisions16_JSON_LowPU.txt'
-sam["data_ZeroBias"]["lumiMinBias"]=-1
-sam["data_ZeroBias"]["XS"]=-1
-sam["data_ZeroBias"]["DS"]='/ZeroBias/Run2016H-03Feb2017_ver3-v1/MINIAOD'
 
 
 def fixLocalPaths(sam):
